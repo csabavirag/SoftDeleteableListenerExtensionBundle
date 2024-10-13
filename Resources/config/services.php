434e9ded94b098d15b9b6b72d8f0436334828f68
Julien Dephix <jdephix@hotmail.com>
2024-10-13 14:12:40 +0200

    Remove deprecated (and unused) containerAwareTrait

--- Resources/config/services.php
+++ Resources/config/services.php
@@ -6,9 +6,6 @@ use Symfony\Component\DependencyInjection\Reference;
 /** @var \Symfony\Component\DependencyInjection\ContainerBuilder $container */
 $container->setDefinition('evence.softdeletale.listener.softdelete', new Definition('Evence\Bundle\SoftDeleteableExtensionBundle\EventListener\SoftDeleteListener', array(new Reference('annotation_reader'))))
 
-->addMethodCall('setContainer', array(
-    new Reference('service_container'),
-))
 ->addTag('doctrine.event_listener', array(
     'event' => 'preSoftDelete',
 ));
