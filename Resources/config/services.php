741936eafc1f4f7f53ec483e35c72a808ac3fea1
RomanApunts <62882223+RomanApunts@users.noreply.github.com>
2024-10-15 16:51:57 +0200

    Update services.php
    Make Doctrine annotations optional

--- Resources/config/services.php
+++ Resources/config/services.php
@@ -2,9 +2,10 @@
 
 use Symfony\Component\DependencyInjection\Definition;
 use Symfony\Component\DependencyInjection\Reference;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /** @var \Symfony\Component\DependencyInjection\ContainerBuilder $container */
-$container->setDefinition('evence.softdeletale.listener.softdelete', new Definition('Evence\Bundle\SoftDeleteableExtensionBundle\EventListener\SoftDeleteListener', array(new Reference('annotation_reader'))))
+$container->setDefinition('evence.softdeletale.listener.softdelete', new Definition('Evence\Bundle\SoftDeleteableExtensionBundle\EventListener\SoftDeleteListener', array(new Reference('annotation_reader', ContainerInterface::NULL_ON_INVALID_REFERENCE))))
 
 ->addTag('doctrine.event_listener', array(
     'event' => 'preSoftDelete',
